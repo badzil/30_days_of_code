ee9b702af51c48e86a3b9f7dc7d2c20274f6ac10
Benoit Thiell <badzil@gmail.com>
2016-07-12 08:40:22 +0200

    Day 2: Operators
    Pretty easy. Starting to get the hang out of Scanf/Printf. Only weird
    thing is that there doesn't seem to be a built-in method to round floats.

---
+++ day_2.go
@@ -0,0 +1,25 @@
+// Solution to https://www.hackerrank.com/challenges/30-operators.
+package main
+
+import (
+    "fmt"
+    "math"
+)    
+
+// Go doesn't know how to round numbers out of the box. Uh.
+// Copied from https://github.com/golang/go/issues/4594#issuecomment-135336012.
+func round(f float64) int {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	return int(f + math.Copysign(0.5, f))
+}
+
+func main() {
+	var price, taxPercent, tipPercent float64
+	fmt.Scanf("%v\n%v\n%v\n", &price, &tipPercent, &taxPercent)
+	tax := price * taxPercent / 100
+	tip := price * tipPercent / 100
+	price += tax + tip
+	fmt.Printf("The total meal cost is %v dollars.", round(price))
+}
