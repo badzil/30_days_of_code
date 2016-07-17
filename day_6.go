164f32531644403c06ee0e1fadf0d6115cd13a3d
Benoit Thiell <badzil@gmail.com>
2016-07-17 12:45:24 +0200

    Day 6: Let's Review
    Nothing difficult. I'm starting to reuse some base functions
    (getProblemSize, safeAtoi) which makes developing the solutions easier.

---
+++ day_6.go
@@ -0,0 +1,44 @@
+package main
+
+import "bufio"
+import "fmt"
+import "log"
+import "os"
+import "strconv"
+import "strings"
+
+func safeAtoi(str string) int {
+        integer, err := strconv.Atoi(strings.TrimSpace(str))
+        if err != nil {
+                log.Fatal(err)
+                os.Exit(1)
+        }
+        return integer
+}
+
+func getProblemSize(buffer *bufio.Reader) int {
+        problem_size_str, _ := buffer.ReadString('\n')
+        return safeAtoi(problem_size_str)
+}
+
+func printOddAndEvenCharacters(word string) {
+    oddChars, evenChars := "", ""
+    for index, character := range word {
+        if (index + 1) % 2 == 1 {
+            oddChars += string(character)
+        } else {
+            evenChars += string(character)
+        }
+    }
+    fmt.Printf("%v %v\n", oddChars, evenChars)
+}
+
+func main() {
+    buffer := bufio.NewReader(os.Stdin)
+    problem_size := getProblemSize(buffer)
+    for i := 0; i < problem_size; i++ {
+        newline, _ := buffer.ReadString('\n')
+        word := strings.TrimSpace(newline)
+        printOddAndEvenCharacters(word)
+    }
+}
