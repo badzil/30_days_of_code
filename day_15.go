7eaf49308325317022cb8b7b33dba13727bc7199
Benoit Thiell <badzil@gmail.com>
2016-07-27 08:44:02 +0200

    gofmt

--- day_15.go
+++ day_15.go
@@ -3,39 +3,39 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Node struct {
-    data int
-    childNode *Node
+	data      int
+	childNode *Node
 }
 
 func (parentNode *Node) Display() {
-    for parentNode != nil {
-        fmt.Printf("%v ", parentNode.data)
-        parentNode = parentNode.childNode
-    }
-    fmt.Println()
+	for parentNode != nil {
+		fmt.Printf("%v ", parentNode.data)
+		parentNode = parentNode.childNode
+	}
+	fmt.Println()
 }
 
 func Insert(head *Node, data int) *Node {
-    if head == nil {
-        head = &Node{data: data}
-    } else {
-        parentNode := head
-        for parentNode.childNode != nil {
-            parentNode = parentNode.childNode
-        }
-        parentNode.childNode = &Node{data: data}
-    }
-    return head
+	if head == nil {
+		head = &Node{data: data}
+	} else {
+		parentNode := head
+		for parentNode.childNode != nil {
+			parentNode = parentNode.childNode
+		}
+		parentNode.childNode = &Node{data: data}
+	}
+	return head
 }
 
 func main() {
-    var head *Node
-    for _, number := range []int{2, 3, 4, 1} {
-        head = Insert(head, number)
-    }
-    head.Display()
+	var head *Node
+	for _, number := range []int{2, 3, 4, 1} {
+		head = Insert(head, number)
+	}
+	head.Display()
 }
